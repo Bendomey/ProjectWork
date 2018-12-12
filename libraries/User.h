d6d7270e0d1e0bfb23550d1ea855f4615f2b5113
NedKhelly <domeybenjamin1@gmail.com>
2018-12-12 14:10:39 -0500

    created the user file

---
+++ libraries/User.h
@@ -0,0 +1,20 @@
+/**
+ * @Author: domey
+ * @Date:   2018-12-11T17:34:25-05:00
+ * @Last modified by:   root
+ * @Last modified time: 2018-12-11T20:22:31-05:00
+ */
+
+
+
+#ifndef USER_H
+#define USER_H
+
+/**
+ * this file is the core for the users(admin, students, staff)
+ */
+
+
+
+
+#endif //USER_H
